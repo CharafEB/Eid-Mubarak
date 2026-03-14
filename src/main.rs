246c12ebdb7d77ad1fffa309523424355f5045f9
CharafEB <achrafgt4@gmail.com>
2026-03-14 02:55:30 +0100

    Creating a simple clone for grrs tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::{Context, Result};
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+fn main() -> Result<()>{
+    let arg = Cli::parse();
+    let content = std::fs::read_to_string(&arg.path).with_context(|| format!("could not read file `{}`", arg.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&arg.pattern){
+            println!("{:?}",line);
+        }
+    }
+
+    Ok(())
 }
